f68e964f0528c16da5802d0083316fcc024fbcb8
camille-sommer <106606900+camille-sommer@users.noreply.github.com>
2022-06-20 20:15:17 -0300

    Update getUserPermission.ts

--- src/middleWare/getUserPermission.ts
+++ src/middleWare/getUserPermission.ts
@@ -1,1 +1,30 @@
-
+import { Request, Response, NextFunction } from "express";
+import { verify } from "jsonwebtoken";
+interface IUser {
+email: string;
+id: string;
+permission: string;
+}
+interface IPayload {
+sub: string;
+user: IUser;
+}
+export function getUserPermission(
+request: Request,
+response: Response,
+next: NextFunction
+) {
+const authToken = request.headers.authorization;
+if (!authToken) {
+request.is_admin = false;
+return next();
+}
+const [, token] = authToken.split(" ");
+try {
+const { user } = verify(token, process.env.JWT_SECRET) as IPayload;
+request.is_admin = user.permission === "admin";
+return next();
+} catch (err) {
+return response.status(401).json({ error: err });
+}
+}
