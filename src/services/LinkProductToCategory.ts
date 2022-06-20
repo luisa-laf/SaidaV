841820215aaeb7106d1cbf2f3c9b8dbfb21817ea
Joana Maggioni <100417975+joanamaggioni@users.noreply.github.com>
2022-06-20 19:37:27 -0300

    Update LinkProductToCategory.ts

--- src/services/LinkProductToCategory.ts
+++ src/services/LinkProductToCategory.ts
@@ -1,1 +1,33 @@
+import prismaClient from "../../prisma";
+
+class LinkProductToCategory {
+  async execute(product_id: string, categories: number[]) {
+    const product = await prismaClient.product.findFirst({
+      where: { id: product_id },
+    });
+
+    if (!product) {
+      throw {
+        error: "Product not found.",
+        code: 404,
+      };
+    }
+
+    for (const category of categories) {
+      const productCategory = await prismaClient.productCategory.findFirst({
+        where: { product_id, category_id: category },
+      });
+
+      if (!productCategory)
+        await prismaClient.productCategory.create({
+          data: {
+            category_id: category,
+            product_id,
+          },
+        });
+    }
+  }
+}
+
+export { LinkProductToCategory };
 
