f52df1b796f037695272c9950ba7112fc1ac2e1d
Joana Maggioni <100417975+joanamaggioni@users.noreply.github.com>
2022-06-20 19:30:50 -0300

    Update DeleteProductService.ts

--- src/services/DeleteProductService.ts
+++ src/services/DeleteProductService.ts
@@ -1,1 +1,33 @@
+import prismaClient from "../../prisma";
+
+class DeleteProductService {
+  async execute(id: string) {
+    try {
+      await prismaClient.quantity.deleteMany({
+        where: {
+          product_id: id,
+        },
+      });
+
+      await prismaClient.productCategory.deleteMany({
+        where: {
+          product_id: id,
+        },
+      });
+
+      await prismaClient.product.delete({
+        where: {
+          id,
+        },
+      });
+    } catch (error) {
+      throw {
+        error: "Product not found.",
+        code: 404,
+      };
+    }
+  }
+}
+
+export { DeleteProductService };
 
